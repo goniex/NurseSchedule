df4d0a87c87ec731c0e2f7de8498c8bf36ac892f
tomek__1990 <t.morek@gmail.com>
2014-12-06 21:46:41 +0100

    Change hibernate configuration. Add CRUD operations for NurseDto - merge

--- src/main/java/com/nurseschedule/mvc/service/INurseService.java
+++ src/main/java/com/nurseschedule/mvc/service/INurseService.java
@@ -2,6 +2,8 @@ package com.nurseschedule.mvc.service;
 
 import com.nurseschedule.mvc.dto.NurseDto;
 
+import java.util.List;
+
 /**
  * @author Tomasz Morek
  */
@@ -13,4 +15,34 @@ public interface INurseService {
      * @return NurseDto
      */
     NurseDto findById(Integer id);
+
+    /**
+     * Get all nurses
+     * @return List
+     */
+    List<NurseDto> findAll();
+
+    /**
+     * Create new nurse
+     * @param email
+     * @param password
+     * @param name
+     * @param lastName
+     * @param workTime
+     * @param type
+     */
+    Integer create(String email, String password, String name,
+                   String lastName, Integer workTime, String type);
+
+    /**
+     * Update existing nurse
+     * @param nurse
+     */
+    void update(NurseDto nurse);
+
+    /**
+     * Delete nurse by ID
+     * @param id
+     */
+    void delete(Integer id);
 }
--- src/test/java/com/nurseschedule/mvc/NurseTest.java
+++ src/test/java/com/nurseschedule/mvc/NurseTest.java
@@ -9,6 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import java.util.List;
+
 /**
  * @author Tomasz Morek
  */
@@ -22,7 +24,41 @@ public class NurseTest {
 
     @Test
     public void testGetNurseById() throws Exception {
-        NurseDto nurse = this.nurseService.findById(1);
+        NurseDto nurse = this.nurseService.findById(25);
         Assert.assertNotNull(nurse);
+    }
+
+    @Test
+    public void testGetAllNurses() throws Exception {
+        List<NurseDto> nurses = this.nurseService.findAll();
+        Assert.assertNotNull(nurses);
+    }
+
+    @Test
+    public void testCreateNurse() throws Exception {
+        Integer id = this.nurseService.create("mail@mail.com", "haslo", "test", "test", 30, "N");
+        Assert.assertNotNull(this.nurseService.findById(id));
+        this.nurseService.delete(id);
+    }
+
+    @Test
+    public void testDeleteNurse() throws Exception {
+        Integer id = this.nurseService.create("mail@mail.com", "haslo", "test", "test", 30, "N");
+        Assert.assertNotNull(this.nurseService.findById(id));
+        NurseDto nurseToDelete = new NurseDto();
+        this.nurseService.delete(id);
+        Assert.assertNull(this.nurseService.findById(id));
+    }
+
+    @Test
+    public void testUpdateNurse() throws Exception {
+        Integer id = this.nurseService.create("mail@mail.com", "haslo", "test", "test", 30, "N");
+        NurseDto nurse = this.nurseService.findById(id);
+        String newName = "Newname";
+        nurse.setName(newName);
+        this.nurseService.update(nurse);
+        nurse = this.nurseService.findById(id);
+        Assert.assertEquals(newName, nurse.getName());
+        this.nurseService.delete(id);
     }
 }
